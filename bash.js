06d1207e591ea64d2e82b73bacdf82d2ff376ad1
Casey Chan <caseygocoding@gmail.com>
2018-01-15 14:51:49 -0500

    half-completed project

---
+++ bash.js
@@ -0,0 +1,21 @@
+var commands = require("./commands.js");
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', function (data) {
+	var cmd = data.toString().trim();
+	cmd = cmd.split(' ');
+	var first = cmd[0];
+	var remaining = cmd.slice(1).join(' ');
+	if (first === 'pwd')  commands.pwd(remaining); 
+	if (first === 'date') commands.date(remaining);
+	if (first === 'ls')   commands.ls(remaining); 
+	if (first === 'echo') commands.echo(remaining);
+	if (first === 'cat')  commands.cat(remaining); 
+	// } else if (first === 'head') {
+	// 	commands.head(remaining);
+	// } else if (first === 'tail') {
+	// 	commands.tail(remaining);
+	// }
+  // process.stdout.write('\nprompt > ');
+});
---
+++ commands.js
@@ -0,0 +1,52 @@
+var fs = require('fs');
+
+var pwd = function(fileName) {
+	process.stdout.write(process.cwd());
+	process.stdout.write("\nprompt > ");
+}
+
+var date = function(fileName) {
+	process.stdout.write(Date());
+	process.stdout.write("\nprompt > ");
+}	
+
+var ls = function(fileName) {
+	fs.readdir('.', function(err, files) {
+	  if (err) throw err;
+	  files.forEach(function(file) {
+	    process.stdout.write(file.toString() + "\n");
+	  })
+	  process.stdout.write("prompt > ");
+	});
+}
+
+var echo = function(fileName) {
+	process.stdout.write(fileName);
+	process.stdout.write("\nprompt > ");
+}
+
+var cat = function(fileName) {
+	fs.readFile(pwd(fileName).toString(), (err, data) => {
+  		if (err) throw err;
+  			console.log(data);
+		});
+}
+
+// var head = function(fileName) {
+// 	fs.readFile(pwd(fileName), (err, data) => {
+//   		if (err) throw err;
+//   			data = data.split('\n').splice(0, 10).join('\n');
+//   			console.log(data);
+// 		});
+// }
+
+// var tail = function(fileName) {
+// 	fs.readFile(pwd(fileName), (err, data) => {
+//   		if (err) throw err;
+//   			console.log(data);
+// 		});
+// }
+
+module.exports = {
+	pwd, date, ls, echo, cat
+};
